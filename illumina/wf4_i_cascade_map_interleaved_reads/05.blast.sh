ad474e3d50b6729cc4e62c0ad45b5186f87a8c8b
Marco De La Pierre <marco.delapierre@gmail.com>
2018-12-12 16:53:13 +0800

    illumina: blast both non rc and rc by default

--- illumina/wf4_i_cascade_map_interleaved_reads/05.blast.sh
+++ illumina/wf4_i_cascade_map_interleaved_reads/05.blast.sh
@@ -26,56 +26,68 @@ blast_cont="quay.io/biocontainers/blast:2.7.1--h96bfa4b_5"
 
 
 # switch for reverse-complement
-revcom="no"
-if [ "$revcom" == "yes" ] ; then
+revcom="both"
+if [ "$revcom" == "both" ] ; then
+ $srun_cmd shifter run $samtools_cont samtools faidx \
+    -i -o $group/contigs_sub_rc.fasta \
+    $group/contigs_sub.fasta $(grep '^>' $group/contigs_sub.fasta | tr -d '>')
+ prefix_contig_list="contigs_sub contigs_sub_rc"
+elif [ "$revcom" == "yes" ] ; then
  $srun_cmd shifter run $samtools_cont samtools faidx \
 	-i -o $group/contigs_sub_rc.fasta \
 	$group/contigs_sub.fasta $(grep '^>' $group/contigs_sub.fasta | tr -d '>')
- prefix_contig="contigs_sub_rc"
+ prefix_contig_list="contigs_sub_rc"
 else
- prefix_contig="contigs_sub"
+ prefix_contig_list="contigs_sub"
 fi
 
-# copying input data to scratch
-for f in ${prefix_contig}.fasta ; do
- cp -p $group/$f $scratch/
+# start loop on standard / reverse-complement
+for prefix_contig in $prefix_contig_list ; do
+ echo "Now running for prefix : $prefix_contig"
+
+ # copying input data to scratch
+ for f in ${prefix_contig}.fasta ; do
+  cp -p $group/$f $scratch/
+ done
+ 
+ # running
+ cd $scratch
+ echo Group directory : $group
+ echo Scratch directory : $scratch
+ echo SLURM job id : $SLURM_JOB_ID
+ 
+ # blasting
+ echo TIME blast start $(date)
+ $srun_cmd shifter run $blast_cont blastn \
+ 	-query ${prefix_contig}.fasta -db /group/data/blast/nt \
+ 	-outfmt 11 -out blast_${prefix_contig}.asn \
+ 	-max_hsps 50 \
+ 	-word_size 28 -evalue 0.1 \
+ 	-reward 1 -penalty -2 \
+ 	-num_threads $OMP_NUM_THREADS
+ echo TIME blast end $(date)
+ 
+ # producing blast output in different formats
+ $srun_cmd shifter run $blast_cont blast_formatter \
+ 	-archive blast_${prefix_contig}.asn \
+ 	-outfmt 5 -out blast_${prefix_contig}.xml
+ echo TIME xml end $(date)
+ 
+ $srun_cmd shifter run $blast_cont blast_formatter \
+ 	-archive blast_${prefix_contig}.asn \
+ 	-outfmt 6 -out blast_unsort_default_${prefix_contig}.tsv
+ echo TIME tsv end $(date)
+ 
+ $srun_cmd shifter run $blast_cont blast_formatter \
+ 	-archive blast_${prefix_contig}.asn \
+ 	-outfmt "6 qaccver saccver pident length evalue bitscore stitle" -out blast_unsort_${prefix_contig}.tsv
+ echo TIME custom tsv end $(date)
+ 
+ sort -n -r -k 6 blast_unsort_${prefix_contig}.tsv >blast_${prefix_contig}.tsv
+ echo TIME sort custom tsv end $(date)
+ 
+ # copying output data back to group
+ cp -p $scratch/blast_${prefix_contig}.tsv $scratch/blast_${prefix_contig}.xml $group/
+
+# end loop on standard / reverse-complement
 done
-
-# running
-cd $scratch
-echo Group directory : $group
-echo Scratch directory : $scratch
-echo SLURM job id : $SLURM_JOB_ID
-
-# blasting
-echo TIME blast start $(date)
-$srun_cmd shifter run $blast_cont blastn \
-	-query ${prefix_contig}.fasta -db /group/data/blast/nt \
-	-outfmt 11 -out blast_${prefix_contig}.asn \
-	-max_hsps 50 \
-	-word_size 28 -evalue 0.1 \
-	-reward 1 -penalty -2 \
-	-num_threads $OMP_NUM_THREADS
-echo TIME blast end $(date)
-
-# producing blast output in different formats
-$srun_cmd shifter run $blast_cont blast_formatter \
-	-archive blast_${prefix_contig}.asn \
-	-outfmt 5 -out blast_${prefix_contig}.xml
-echo TIME xml end $(date)
-
-$srun_cmd shifter run $blast_cont blast_formatter \
-	-archive blast_${prefix_contig}.asn \
-	-outfmt 6 -out blast_unsort_default_${prefix_contig}.tsv
-echo TIME tsv end $(date)
-
-$srun_cmd shifter run $blast_cont blast_formatter \
-	-archive blast_${prefix_contig}.asn \
-	-outfmt "6 qaccver saccver pident length evalue bitscore stitle" -out blast_unsort_${prefix_contig}.tsv
-echo TIME custom tsv end $(date)
-
-sort -n -r -k 6 blast_unsort_${prefix_contig}.tsv >blast_${prefix_contig}.tsv
-echo TIME sort custom tsv end $(date)
-
-# copying output data back to group
-cp -p $scratch/blast_${prefix_contig}.tsv $scratch/blast_${prefix_contig}.xml $group/
